0fea562f1a637a08f0f5edc6f94c88d8061267e0
brooks-anderson <gcu6hc@virginia.edu>
2022-03-11 16:17:44 -0500

    forgot to push

--- dataframe_builder.py
+++ dataframe_builder.py
@@ -31,5 +31,31 @@ DC_shape = gpd.GeoDataFrame({'city_name': city,
 # write to .shp
 DC_shape.to_file("data/DC_boundary.shp", driver = 'ESRI Shapefile')
 
+#######################
+
+# Create litter can dataframe
+can_URL = "https://opendata.arcgis.com/datasets/4c9d7e966c0f435485fe0f47144c3258_10.geojson"
+
+can_response = requests.get(can_URL)
+
+can_list = []
+geom_list = []
+
+for i in range(len(can_response.json()['features'])):
+    obj_id = can_response.json()['features'][i]['properties']['OBJECTID']
+    rat = can_response.json()['features'][i]['properties']['RAT_COVER']
+    wash = can_response.json()['features'][i]['properties']['POWERWASH']
+    geom = Point(can_response.json()['features'][i]['geometry']['coordinates'])
+    
+    can_list.append({'object_id' : obj_id,
+                     'rat_cover' : rat,
+                     'power_wash' : wash})
+    geom_list.append(geom)
+
+can_df = pd.DataFrame(can_list)
+
+litter_cans = gpd.GeoDataFrame(can_df.copy(), geometry = geom_list,)
+
+litter_cans.to_file("data/litter_cans.shp", driver = 'ESRI Shapefile')
 
 
